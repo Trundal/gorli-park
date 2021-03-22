042becdffbcb70160c919d14119f9e2646618bf2
Korey Powell <korey@vagabonddream.com>
2021-03-22 20:19:52 +0100

    About Us

---
+++ pages/about.js
@@ -0,0 +1,41 @@
+import Container from "../components/Container/Container";
+import Bio from "../components/Bio/Bio";
+import Layout from "../components/Layout/Layout";
+import { getSite, getAbout } from "../lib/api";
+import Head from "next/head";
+
+export default function Index({ preview, about, bgGraphic }) {
+  console.log(about);
+
+  return (
+    <Layout preview={preview} bgGraphic={bgGraphic}>
+      <Head>
+        <title>GorliPark - Imprint</title>
+      </Head>
+      <Container>
+        { about.map(bio => 
+          <Bio 
+            bio={bio.bio}
+            name={bio.name}
+            title={bio.title}
+            image={bio.image}
+          />
+        )}
+      </Container>
+    </Layout>
+  );
+}
+
+export async function getStaticProps({ preview = false }) {
+  const site = await getSite();
+  const about = await getAbout();
+
+  return {
+    props: {
+      preview,
+      about,
+      bgGraphic: site?.bgGraphic || null,
+    },
+    revalidate: 1,
+  };
+}
