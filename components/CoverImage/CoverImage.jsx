d54fa515685aa3d3b1cd30d850639294835ce960
Zackarias Branzell <mrbranzell@Zackariass-MacBook-Pro.local>
2021-04-28 13:29:53 +0200

    hopfully that was all the images now with scrSet yo

--- components/CoverImage/CoverImage.jsx
+++ components/CoverImage/CoverImage.jsx
@@ -7,11 +7,23 @@ import styles from './CoverImage.module.css';
 export default function CoverImage({ title, imageObject, slug, pageType }) {
   const image = (
     <img
-      style={{ width: '100%' }}
+      style={{
+        width: '100%',
+        // backgroundImage: `url(${imageBuilder(imageObject).metadata.lqip})`,
+        // paddingTop: `calc(100% / ${imageBuilder(imageObject).metadata.dimensions.aspectRatio})`,
+      }}
       alt={`Cover Image for ${title}`}
       className={cn('', {
         '': slug,
       })}
+      srcSet={
+        (`${imageBuilder(imageObject).width(1920).url()} 1920px`,
+        `${imageBuilder(imageObject).width(1600).url()} 1600px`,
+        `${imageBuilder(imageObject).width(1366).url()} 1366px`,
+        `${imageBuilder(imageObject).width(1024).url()} 1024px`,
+        `${imageBuilder(imageObject).width(768).url()} 768px`,
+        `${imageBuilder(imageObject).width(640).url()} 640px`)
+      }
       src={imageBuilder(imageObject).width(1240).height(540).url()}
     />
   );
